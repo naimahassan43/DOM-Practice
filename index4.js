ea6b9a0c3a14db7dd702234ea7e7d049948d7a0b
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-28 00:45:57 +0100

    Various Event Listeners

---
+++ index4.js
@@ -0,0 +1,27 @@
+/********* Various Event Listeners *******/
+
+const clearBtn = document.querySelector('.clear-post');
+
+// clearBtn.addEventListener('dblclick', function(event) {
+//     console.log('Hello!');
+// });
+
+// clearBtn.addEventListener('mousedown', function(event) {
+//     console.log('Hello!');
+// });
+
+// clearBtn.addEventListener('mouseup', function(event) {
+//     console.log('Hello!');
+// });
+
+// clearBtn.addEventListener('mouseenter', function(event) {
+//     console.log('Hello!');
+// });
+
+// clearBtn.addEventListener('mouseleave', function(event) {
+//     console.log('Hello!');
+// });
+
+clearBtn.addEventListener('mousemove', function(event) {
+    console.log('Hello!');
+});
