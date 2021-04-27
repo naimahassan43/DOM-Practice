0289804c5fd8d04a006d3b0cc3b40d736b202b10
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-27 21:46:52 +0100

    Observing Document Object

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// I wish you good luck and happy coding 🥰🤠🥳🥳💯💯
+// Observing Document Object
+console.log(document);
+console.log(document.all);
+console.log(document.all[1]);
+console.log(document.all[7]);
+console.log(document.all.length);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.charset);
+console.log(document.contentType);
+
+console.log(document.body.className);
+console.log(document.body.classList);
+
+console.log(document.forms);
+console.log(document.forms[0]);
+console.log(document.forms[0].id);
+console.log(document.forms[0].className);
+console.log(document.forms[0].action);
+console.log(document.forms[0].method);
+
+console.log(document.links);
+
+console.log(document.scripts);
+console.log(document.scripts[3].getAttribute('src'));
+
+let scripts = Array.from(document.scripts);
+console.log(scripts);
+
+scripts.forEach((script) => console.log(script.getAttribute('src')));
