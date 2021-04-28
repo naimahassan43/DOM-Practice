b60c5a2b8bcf197e182ea50a8f637657af4fb0fe
Syeda Naima Hassan <naima.hassan434@gmail.com>
2021-04-28 01:08:22 +0100

    Observing Event Object

--- index4.js
+++ index4.js
@@ -22,6 +22,29 @@ const clearBtn = document.querySelector('.clear-post');
 //     console.log('Hello!');
 // });
 
-clearBtn.addEventListener('mousemove', function(event) {
-    console.log('Hello!');
+// clearBtn.addEventListener('mousemove', function(event) {
+//     console.log('Hello!');
+// });
+
+/********* Observing Event Object *******/
+
+clearBtn.addEventListener('click', (event) => {
+    // console.log(event);
+
+    // console.log(event.clientX);
+    // console.log(event.clientY);
+    // console.log(event.offsetX);
+
+    // console.log(event.altKey);
+    // console.log(event.shiftKey);
+    // console.log(event.ctrlKey);
+
+    console.log(event.type);
+
+    console.log(event.target);
+    console.log(event.target.id);
+    console.log(event.target.className);
+    console.log(event.target.innerText);
+
+    event.target.style.background = "#000";
 });
